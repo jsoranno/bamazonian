db95921dbac41a295fb06e056790270adf7fabc7
Juanita Soranno <juanitasoranno@gmail.com>
2016-10-24 22:59:10 -0400

    Added modules

---
+++ main.js
@@ -0,0 +1,19 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "rsl7sn8bro1pqf5w",
+    password: "jgyzqodd0qael81m",
+    database: "f0yv6chbblwd6olh"
+
+});
+
+connection.connect(function(err){
+    if(err)
+    {
+        throw err;
+    }
+    console.log("connected as id " + connection.threadId);
+});
